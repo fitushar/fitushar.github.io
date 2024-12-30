5272fd72ff8a9510415e9aff68c849a154d07dbe
fitushar <tushar_khan33@hotmail.com>
2024-12-30 05:16:30 +0000

    Deploying to gh-pages from @ fitushar/fitushar.github.io@8f87994d760260f54863f12b820321e6c6bac8d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,9 +406,9 @@ ninja.data = [{
           description: "Virtual Imaging Trials Improved the Transparency and Reliability of AI Systems",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-i-predict",
+          title: 'I-Predict',
+          description: "Weakly Supervised Classification",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
