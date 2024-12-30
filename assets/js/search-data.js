204e3832a147316e0808e3ee04048b575f16b4b1
fitushar <tushar_khan33@hotmail.com>
2024-12-30 06:04:53 +0000

    Deploying to gh-pages from @ fitushar/fitushar.github.io@28b0fe6684fbf6793ed104f0f1c13cae9e50bedf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,7 +413,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-lung-cancer-longitudinal-ai-with-rule-based-interpretability-clarity",
           title: 'Lung Cancer Longitudinal AI with Rule-Based Interpretability (CLARITY)',
-          description: "Development of a foundational-AI with Synthetic In Silico longitudinal Digital Humans.",
+          description: "Development of a foundational AI with Synthetic In Silico longitudinal Digital Humans.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
