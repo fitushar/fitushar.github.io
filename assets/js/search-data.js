f8b94a3fc677efd1ab2721aff9fdf097beddb074
fitushar <tushar_khan33@hotmail.com>
2024-12-30 04:56:10 +0000

    Deploying to gh-pages from @ fitushar/fitushar.github.io@015601ecc75a030592955f37d07e7ac859afeecf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -401,9 +401,9 @@ ninja.data = [{
           description: "Benchmarking Detection, Diagnostic, Segmentation and Radiomics-based analysis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
