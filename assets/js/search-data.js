ecc9bcbff0c8000e75d56ba104ad662d399912c9
fitushar <tushar_khan33@hotmail.com>
2024-12-30 05:32:27 +0000

    Deploying to gh-pages from @ fitushar/fitushar.github.io@deffe18d53d78759eca5bdb89afcbcbd1f5c771e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -411,9 +411,9 @@ ninja.data = [{
           description: "Weakly Supervised Classification",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-lung-cancer-longitudinal-ai-with-rule-based-interpretability-clarity",
+          title: 'Lung Cancer Longitudinal AI with Rule-Based Interpretability (CLARITY)',
+          description: "Development of a foundational-AI with Synthetic In Silico longitudinal Digital Humans.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
