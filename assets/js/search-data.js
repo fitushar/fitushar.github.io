d7e5df0ec421597424538c9935d0d61e5f2af736
fitushar <tushar_khan33@hotmail.com>
2026-03-31 04:36:39 +0000

    Deploying to gh-pages from @ fitushar/fitushar.github.io@f71b30f62d197f58fe68dd90e92582b919487784 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
